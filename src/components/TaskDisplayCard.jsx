5f16cdf33f7c1a9872d70f90d3ee81c0831589e0
faithgaiciumia <faithgaiciumia078@gmail.com>
2025-02-19 14:23:16 +0300

    added view task modal

--- src/components/TaskDisplayCard.jsx
+++ src/components/TaskDisplayCard.jsx
@@ -1,7 +1,19 @@
 /* eslint-disable react/prop-types */
-import { Box, Heading, Text } from "@chakra-ui/react";
+import {
+  Box,
+  Heading,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalHeader,
+  ModalOverlay,
+  Text,
+  useDisclosure,
+} from "@chakra-ui/react";
 
 export default function TaskDisplayCard({ taskTitle, subTasks }) {
+  const { onOpen, isOpen, onClose } = useDisclosure();
   return (
     <Box
       backgroundColor={"white"}
@@ -10,6 +22,10 @@ export default function TaskDisplayCard({ taskTitle, subTasks }) {
       my={4}
       w={"100%"}
       boxShadow={"lg"}
+      cursor={"pointer"}
+      transition={"transform 0.2s ease-in-out"}
+      _hover={{ transform: "scale(1.1)", boxShadow: "xl" }}
+      onClick={onOpen}
     >
       <Box>
         <Heading
@@ -22,6 +38,16 @@ export default function TaskDisplayCard({ taskTitle, subTasks }) {
           0 of {subTasks.length} subtasks
         </Text>
       </Box>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Task Name</ModalHeader>
+          <ModalCloseButton variant={"ghost"} />
+          <ModalBody>
+            <Text>Description</Text>
+          </ModalBody>
+        </ModalContent>
+      </Modal>
     </Box>
   );
 }
